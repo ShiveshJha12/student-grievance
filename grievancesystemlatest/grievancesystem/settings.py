5bd54a4578dfeaabe15fa9d78e4009b23529e3fe
Sanay Shah <sanayshah2@gmail.com>
2020-06-09 00:42:05 +0530

    initial commit

--- grievancesystemlatest/grievancesystem/settings.py
+++ grievancesystemlatest/grievancesystem/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,3 +133,6 @@ EMAIL_HOST_USER = 'studentgrievance69@gmail.com'
 EMAIL_HOST_PASSWORD = 'admin6969'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
